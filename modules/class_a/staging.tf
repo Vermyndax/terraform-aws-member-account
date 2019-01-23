2c415bd9d3734297ad00a22c4e9e533ce0422007
Jason Miller <jmiller@red-abstract.com>
2019-01-23 17:02:14 -0600

    Add CodeBuild roles

--- modules/class_a/staging.tf
+++ modules/class_a/staging.tf
@@ -167,6 +167,129 @@ EOF
   # )}"
 
 }
+
+resource "aws_iam_role" "staging_codebuild_role" {
+  provider = "aws.staging"
+  count = "${var.create_pipelines == "true" ? 1 : 0 }"
+  name = "${var.tag_application_id}-staging-codebuild-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+  tags = "${merge(
+    local.required_tags,
+    map(
+      "Environment", "staging",
+    )
+  )}"
+}
+
+resource "aws_iam_role_policy" "staging_codebuild_policy" {
+  provider = "aws.staging"
+  count = "${var.create_pipelines == "true" ? 1 : 0 }"
+  name = "${var.tag_application_id}-staging-codebuild-policy"
+  role = "${aws_iam_role.staging_codebuild_role.id}"
+
+policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+       "s3:PutObject",
+       "s3:GetObject",
+       "s3:GetObjectVersion",
+       "s3:GetBucketVersioning"
+      ],
+      "Resource": "*",
+      "Effect": "Allow"
+    },
+    {
+      "Action": [
+          "s3:*"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.staging_codepipeline_artifact_bucket.arn}",
+        "${aws_s3_bucket.staging_codepipeline_artifact_bucket.arn}/*",
+        "${aws_s3_bucket.staging_terraform_state_bucket.arn}",
+        "${aws_s3_bucket.staging_terraform_state_bucket.arn}/*"
+      ],
+      "Effect": "Allow"
+    },
+    {
+      "Action": [
+        "codebuild:*"
+      ],
+      "Resource": [
+        "${aws_codebuild_project.staging_provision.id}",
+        "${aws_codebuild_project.git_merge_staging_to_master.id}"
+      ],
+      "Effect": "Allow"
+    },
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": [
+        "*"
+      ],
+      "Effect": "Allow"
+    },
+    {
+      "Action": [
+        "kms:DescribeKey",
+        "kms:GenerateDataKey*",
+        "kms:Encrypt",
+        "kms:ReEncrypt*",
+        "kms:Decrypt"
+      ],
+      "Resource": [
+        "${aws_kms_key.staging_s3_kms_key.arn}"
+        ],
+      "Effect": "Allow"
+    },
+    {
+      "Action": [
+        "sns:Publish"
+      ],
+      "Resource": "${aws_sns_topic.staging_sns_topic.arn}",
+      "Effect": "Allow"
+    },
+    {
+      "Action": [
+        "ec2:*",
+        "kms:CreateKey"
+      ],
+      "Resource": "*",
+      "Effect": "Allow"
+    }
+  ]
+}
+POLICY
+
+  # tags = "${merge(
+  #   local.required_tags,
+  #   map(
+  #     "Environment", "staging",
+  #   )
+  # )}"
+
+}
+
 resource "aws_sns_topic" "staging_sns_topic" {
   provider = "aws.staging"
   count = "${var.create_sns_topic == "true" ? 1 : 0}"
