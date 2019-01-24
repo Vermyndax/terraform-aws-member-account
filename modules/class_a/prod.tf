c8464d9916eb339a1bf89c328dca0df55da5fd0e
Jason Miller <jmiller@red-abstract.com>
2019-01-23 23:00:32 -0600

    Setting things hopefully right

--- modules/class_a/prod.tf
+++ modules/class_a/prod.tf
@@ -44,31 +44,31 @@ resource "aws_kms_alias" "prod_s3_kms_key_name" {
   target_key_id = "${aws_kms_key.prod_s3_kms_key.key_id}"
 }
 
-# resource "aws_s3_bucket" "prod_terraform_state_bucket" {
-#   provider = "aws.prod"
-#   count = "${var.create_terraform_state_buckets == "true" ? 1 : 0}"
-#   bucket = "${local.application}-prod-terraform-state"
+resource "aws_s3_bucket" "prod_terraform_state_bucket" {
+  provider = "aws.prod"
+  count = "${var.create_terraform_state_buckets == "true" ? 1 : 0}"
+  bucket = "${local.application}-prod-terraform-state"
 
-#   versioning {
-#     enabled = true
-#   }
+  versioning {
+    enabled = true
+  }
 
-#   server_side_encryption_configuration {
-#     rule {
-#       apply_server_side_encryption_by_default {
-#         kms_master_key_id = "${aws_kms_key.prod_s3_kms_key.arn}"
-#         sse_algorithm     = "aws:kms"
-#       }
-#     }
-#   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.prod_s3_kms_key.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 
-#   tags = "${merge(
-#     local.required_tags,
-#     map(
-#       "Environment", "prod",
-#     )
-#   )}"
-# }
+  tags = "${merge(
+    local.required_tags,
+    map(
+      "Environment", "prod",
+    )
+  )}"
+}
 
 resource "aws_s3_bucket" "prod_codepipeline_artifact_bucket" {
   provider = "aws.prod"
@@ -252,7 +252,9 @@ policy = <<POLICY
       ],
       "Resource": [
         "${aws_s3_bucket.prod_codepipeline_artifact_bucket.arn}",
-        "${aws_s3_bucket.prod_codepipeline_artifact_bucket.arn}/*"
+        "${aws_s3_bucket.prod_codepipeline_artifact_bucket.arn}/*",
+        "${aws_s3_bucket.prod_terraform_state_bucket.arn}",
+        "${aws_s3_bucket.prod_terraform_state_bucket.arn}/*"
       ],
       "Effect": "Allow"
     },
@@ -348,7 +350,7 @@ phases:
   build:
     commands:
       - cd $CODEBUILD_SRC_DIR/terraform
-      - terraform init -backend=true -backend-config="bucket=ra-test-prod-terraform-state" -backend-config="key=${var.terraform_state_file}" -backend-config="region=${var.region}" -no-color
+      - terraform init -backend=true -backend-config="bucket=${aws_s3_bucket.prod_terraform_state_bucket.id}" -backend-config="key=${var.terraform_state_file}" -backend-config="region=${var.region}" -no-color
       - terraform plan -no-color
       - terraform apply -auto-approve -no-color
 BUILDSPEC
